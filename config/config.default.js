0ef50d5f2e62f755cf71612da9efae20ac7090b1
developerlin123 <developerlin123@gmail.com>
2021-07-14 16:42:14 +0200

    change port

--- config/config.default.js
+++ config/config.default.js
@@ -53,8 +53,8 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: process.env.PORT || 7001,
-      hostname: "127.0.0.1",
+      port: process.env.PORT || 8000,
+      hostname: "0.0.0.0",
     }
   };
 
